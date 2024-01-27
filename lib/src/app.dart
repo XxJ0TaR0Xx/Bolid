e07c625620e1013305df79283e88c0b44459f04c
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-01-27 18:34:17 +0300

    fear: add BlocProvide in MainApp

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,21 @@
+import 'package:bolid/core/services.dart';
+import 'package:bolid/src/presentation/controllers/bloc/get_senser_bloc.dart';
 import 'package:bolid/src/presentation/pages/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
-      home: const MainPage(),
+    return BlocProvider(
+      create: (context) => services<GetSenserBloc>(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(useMaterial3: true),
+        home: const MainPage(),
+      ),
     );
   }
 }
