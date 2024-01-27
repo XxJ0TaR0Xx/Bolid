66a22b24c334a809ad13915a6def421fdeaeea18
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-01-27 18:35:00 +0300

    feat: change name and add _onChangeNameSensorEvent

--- lib/src/presentation/controllers/bloc/get_senser_bloc.dart
+++ lib/src/presentation/controllers/bloc/get_senser_bloc.dart
@@ -13,8 +13,9 @@ part 'get_senser_event.dart';
 part 'get_senser_state.dart';
 
 class GetSenserBloc extends Bloc<GetSenserEvent, GetSenserState> {
-  GetSenserBloc() : super(GetSenserInitial()) {
+  GetSenserBloc() : super(InitialGetSenser()) {
     on<InitGetSenserEvent>(_onInitGetSenserEvent);
+    on<ChangeNameSensorEvent>(_onChangeNameSensorEvent);
   }
 
   FutureOr<void> _onInitGetSenserEvent(
@@ -27,27 +28,58 @@ class GetSenserBloc extends Bloc<GetSenserEvent, GetSenserState> {
     serverOrFailure0.fold(
       (failure) {
         emit(
-          GetSenserError(
+          ErrorGetSenser(
             errorStr: failure.toString(),
           ),
         );
       },
       (list) {
-        final int itemCount = list.length;
-        final List<bool> expandableState = List.generate(itemCount, (index) => false);
-        final List<TextEditingController> textEditingControllers = List.generate(itemCount, (index) => TextEditingController());
-        final List<bool> isEditingStates = List.generate(itemCount, (index) => false);
+        final List<TextEditingController> textEditingControllers = List.generate(list.length, (index) => TextEditingController());
+        final List<bool> expandableState = List.generate(list.length, (index) => false);
+        final List<bool> isEditingStates = List.generate(list.length, (index) => false);
+
+        List<SensorInfo> listSensorInfo = [];
+        int index = 0;
+
+        for (var e in list) {
+          listSensorInfo.add(
+            SensorInfo(
+              textEditingController: textEditingControllers[index],
+              isEditingStates: isEditingStates[index],
+              model: SensorModel(
+                name: e.name,
+                sensorId: e.sensorId,
+                status: e.status,
+                humidity: e.humidity,
+                temperature: e.temperature,
+              ),
+              expandableState: expandableState[index],
+            ),
+          );
+          index++;
+        }
 
         emit(
-          GetSenser(
-            listSensorModel: list,
-            expandableState: expandableState,
-            itemCount: itemCount,
-            textEditingControllers: textEditingControllers,
-            isEditingStates: isEditingStates,
-          ),
+          SensorModelGetSenser(listSensorInfo: listSensorInfo),
         );
       },
     );
+  }
+
+  FutureOr<void> _onChangeNameSensorEvent(
+    ChangeNameSensorEvent event,
+    Emitter<GetSenserState> emit,
+  ) {
+    final currentState = (state as SensorModelGetSenser);
+    final oldName = currentState.listSensorInfo[event.index].model.name;
+    if (event.newName != oldName) {
+      print("value name old ${currentState.listSensorInfo[event.index].model.name.toString()}");
+
+      SensorModel sensorModel = currentState.listSensorInfo[event.index].model.copyWith(name: event.newName);
+      currentState.listSensorInfo[event.index].model = sensorModel;
+      print("value name new ${currentState.listSensorInfo[event.index].model.name.toString()}");
+
+      emit(SensorModelGetSenser(listSensorInfo: currentState.listSensorInfo));
+    }
   }
 }
